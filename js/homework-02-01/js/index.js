5d13e929715b722271ee6ddb5ed428ba85f17dd5
Lilia Garshina <peacelovemilk99@gmail.com>
2018-09-07 09:21:14 +0300

    js1

--- js/homework-02-01/js/index.js
+++ js/homework-02-01/js/index.js
@@ -1,1 +1,21 @@
 'use strict'
+
+const adminLogin = 'admin';
+const adminPassword = 'm4ngo1zh4ackz0r';
+
+const loginMessage = prompt('Введите логин:');
+if (loginMessage === null) {
+    alert('Отменено пользователем!');
+} else if (loginMessage !== adminLogin) {
+    alert('Доступ запрещен!')
+} else if (loginMessage === adminLogin) {
+    const passwordMessage = prompt('Введите пароль:');
+    if (passwordMessage === null) {
+        alert('Отменено пользователем!');
+    } else if (passwordMessage !== adminPassword) {
+        alert('Доступ запрещен!');
+    } else if (passwordMessage === adminPassword) {
+        alert('Добро пожаловать!')
+    }
+
+}
